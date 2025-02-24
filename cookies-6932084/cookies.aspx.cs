2cb7349b4121dea5da8568d1b8a0f86d01811494
nathamaya <nathamayaa@outlook.com>
2025-02-24 16:30:33 -0600

    Primer commit en Cookies

---
+++ cookies-6932084/cookies.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies_6932084
+{
+    public partial class cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+            // Save the cookies.
+            Response.Cookies["ddlCategory"].Value = ddlCategory.SelectedValue;
+            Response.Cookies["ddlSupplier"].Value = ddlSupplier.SelectedValue;
+            Response.Cookies["strProduct"].Value = txtProduct.Text;
+            Response.Cookies["strDescription"].Value = txtDescription.Text;
+            Response.Cookies["strImage"].Value = txtImage.Text;
+            Response.Cookies["decPrice"].Value = txtPrice.Text;
+            Response.Cookies["bytNumberInStock"].Value = txtNumberInStock.Text;
+            Response.Cookies["bytNumberOnOrder"].Value = txtNumberOnOrder.Text;
+            Response.Cookies["bytReorderLevel"].Value = txtReorderLevel.Text;
+            // Go to the confirmation page.
+            Response.Redirect("ProductConfirmCookies.aspx");    
+        }
+    }
+}
